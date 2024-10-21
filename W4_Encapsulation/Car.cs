f2e0c9828dd254628e43813972f06bbeab51aa90
AenuHub <jesuskane92@gmail.com>
2024-10-21 16:15:28 +0300

    Add project files.

---
+++ W4_Encapsulation/Car.cs
@@ -0,0 +1,39 @@
+﻿namespace W4_Encapsulation
+{
+    public class Car
+    {
+        private string _make;
+        private string _model;
+        private string _color;
+        private int _numberOfDoors;
+
+        // getters and setters
+        public string Make { get { return _make; } set { _make = value; } }
+        public string Model { get { return _model; } set { _model = value; } }
+        public string Color { get { return _color; } set { _color = value; } }
+        public int NumberOfDoors
+        {
+            get { return _numberOfDoors; }
+            set
+            {
+                if (value != 2 && value != 4)
+                {
+                    Console.WriteLine("Invalid number of doors");
+                    _numberOfDoors = -1;
+                }
+                else
+                {
+                    _numberOfDoors = value;
+                }
+            }
+        }
+
+        public void PrintCarInfo()
+        {
+            Console.WriteLine($"Make: {_make}");
+            Console.WriteLine($"Model: {_model}");
+            Console.WriteLine($"Color: {_color}");
+            Console.WriteLine($"Number of doors: {_numberOfDoors}");
+        }
+    }
+}
---
+++ W4_Encapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using W4_Encapsulation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.Make = "Toyota";
+        car.Model = "Corolla";
+        car.Color = "Red";
+        car.NumberOfDoors = 3;
+
+        car.PrintCarInfo();
+        Console.ReadKey();
+    }
+}
